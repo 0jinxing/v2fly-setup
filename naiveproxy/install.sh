5051c9740db59fe73c03ed703098f1eb3c95e378
Jinxing Lin <172601673@qq.com>
2024-06-07 10:05:07 +0800

    feat: Add naiveproxy configuration files and installation script

---
+++ naiveproxy/install.sh
@@ -0,0 +1,17 @@
+curl -LO https://github.com/klzgrad/forwardproxy/releases/download/v2.7.6-naive2/caddy-forwardproxy-naive.tar.xz
+mkdir -p /usr/local/etc/caddy
+mkdir -p /var/www/html
+tar -xvf caddy-forwardproxy-naive.tar.xz -C /usr/local/etc/caddy
+
+# prompt for username and password
+echo "Please enter your username and password for the proxy"
+
+read -p "Username: " username
+read -sp "Password: " password
+
+echo "Please enter your address name:"
+read address
+
+cat ./Caddyfile | sed "s/%ADDRESS%/$address/g" | sed "s/%EMAIL%/$email/g"  | sed "s/%PASSWORD%/$password/g"  > /usr/local/etc/caddy/Caddyfile
+
+
